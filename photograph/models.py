c146e5d6a887184352e0a2e98ee9d6232c5aaac9
Carl Yebes <yebes77gmail.com>
2020-07-02 23:24:39 +0800

    second commit

--- photograph/models.py
+++ photograph/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class picture(models.Model):
+    title = models.CharField(max_length=50, blank=True)
+    description = models.TextField(max_length=200, blank=True)
+    image = models.ImageField()
