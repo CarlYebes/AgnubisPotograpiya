84d4d1cccda8adf74b859bc7e7bb7f5ec5fdacbf
Carl Yebes <yebes77gmail.com>
2020-07-12 15:31:54 +0800

    added pass for security

--- Photography/settings.py
+++ Photography/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 import django_heroku
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -139,8 +139,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'yebes77@gmail.com'
-EMAIL_HOST_PASSWORD = 'Yebes!123'
+EMAIL_HOST_USER = config('gmail')
+EMAIL_HOST_PASSWORD = config('pass')
 EMAIL_USE_TLS = True
 
 django_heroku.settings(locals())
